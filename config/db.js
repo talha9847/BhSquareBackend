f765aec6ddab323c4ade864fe56676ab79e1ef10
talha9847 <cse.210840131054@gmail.com>
2026-03-13 00:22:07 +0530

    change .env

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
+  dialect: 'postgres',
   dialectOptions: {
-    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   },
   logging: false,
 });
+
+sequelize.authenticate()
+  .then(() => console.log('✅ Connected to Neon!'))
+  .catch(err => console.error('❌ DB connection failed:', err));
 
 module.exports = sequelize;
