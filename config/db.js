f7d223aec0e59d0e10df0f3d9935793d6cf5af33
talha9847 <cse.210840131054@gmail.com>
2026-03-13 00:12:24 +0530

    change .env

--- config/db.js
+++ config/db.js
@@ -1,20 +1,12 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl:
-        process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-    },
-    logging: false, // optional: disable console logs
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
   },
-);
+  logging: false,
+});
 
 module.exports = sequelize;
